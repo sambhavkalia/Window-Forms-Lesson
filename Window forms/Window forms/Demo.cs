189ff3d5e5f9829fa1cb1e5aa7f24597bbdd3048
sambhavkalia <sambhavkalia@gmail.com>
2017-08-01 15:29:25 -0400

    Created Demo Application

---
+++ Window forms/Window forms/Demo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Name: Sambahv kalia
+ * Dae: August 1,2017
+ * Description: This is demo Application to showcase User Interface
+ * Version 0.1 -Created the project
+ * 
+ */
+
+namespace Window_forms
+{
+    public partial class Demo : Form
+    {
+        public Demo()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Window forms/Window forms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Name: Sambahv kalia
+ * Dae: August 1,2017
+ * Description: This is demo Application to showcase User Interface
+ * Version 0.1 -Created the project
+ * 
+ */
+namespace Window_forms
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Demo());
+        }
+    }
+}
